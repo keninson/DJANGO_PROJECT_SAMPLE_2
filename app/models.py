7f0e2eadb73bfda046ea1fdfd9807111991a9b67
keninson <eduard.korolevskiy@gmail.com>
2024-03-12 23:38:06 +0300

    initial

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    name = models.CharField('название', max_length=100)
+    description = models.TextField('описание')
+    team_quantity = models.IntegerField('Количество учасников')
+
+    def __str__(self):
+        return self.name
